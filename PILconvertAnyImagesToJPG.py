73c2129c98cb891033f87a0f68d1b7adc5a27b9c
Ultharas <ultharas@gmail.com>
2016-03-09 13:03:48 +0200

    change names and beautify

--- PILconvertAnyImagesToJPG.py
+++ PILconvertAnyImagesToJPG.py
@@ -17,26 +17,28 @@ def image_post_open(filename):
             im.load()
             return im
 
-def ConvertAllImagesTo(path, new_ext):
-    tif_list = os.listdir(path)
-    for tif_image in tif_list:
+def ConvertAllImagesTo(new_ext):
+    image_list = os.listdir('import')
+    for image in image_list:
 
-        old_ext = re.search('[^\.]+$', tif_image).group(0)
+        old_ext = re.search('[^\.]+$', image).group(0)
         if old_ext == new_ext:
             continue
 
-        tif_path = os.path.join(path, tif_image)
+        image_path = os.path.join('import', image)
 
-        if imghdr.what(tif_path):
+        if imghdr.what(image_path):
 
-            im = image_post_open(tif_path)
+            im = image_post_open(image_path)
 
             if im:
 
                 if im.mode == "CMYK":
                     im = im.convert("RGB")
 
-                save_img_path = os.path.join(path, re.sub('[^\.]+$', new_ext, tif_image))
+                save_img_path = os.path.join('export', re.sub('[^\.]+$', new_ext, image_path))
+                print save_img_path
+                sys.exit()
                 try:
                     im.save(save_img_path)
                 except Exception as e:
@@ -44,4 +46,4 @@ def ConvertAllImagesTo(path, new_ext):
                 else:
                     os.remove(tif_path)
 
-ConvertAllImagesTo('export', 'jpg')
+ConvertAllImagesTo('jpg')
